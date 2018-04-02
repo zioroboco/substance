3e492b145c2700c6ed67165943b356526124ce76
Louis <louis@loop.io>
2018-04-03 00:07:13 +1000

    Set up webpack and base react app
    Using webpack 4 and react-hot-loader 4, both still in early days. Hot loading, source maps and debugging are all working.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { hot } from 'react-hot-loader'
+
+const App = (): JSX.Element => {
+  return (
+    <div style={{textAlign: 'center', fontSize: '10rem'}}>
+      ⬡
+    </div>
+  )
+}
+
+export default hot(module)(App)
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'src/index.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loaders: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                'babel-plugin-syntax-typescript',
+                'babel-plugin-syntax-decorators',
+                'babel-plugin-syntax-jsx',
+                'react-hot-loader/babel'
+              ]
+            }
+          },
+          {
+            loader: 'ts-loader'
+          }
+        ],
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.tsx', '.js', '.jsx' ]
+  },
+  devtool: 'eval-source-map',
+  output: {
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    host: 'localhost',
+    port: '3737',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html')
+    }),
+  ]
+}
